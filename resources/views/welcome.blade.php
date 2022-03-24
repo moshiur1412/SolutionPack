9ecdf01807d267be8707d5fa4c6bb4127043ee61
vorsurm <moshiur1412@gmail.com>
2022-03-24 13:59:53 +0600

    image size changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 	<title>I Seek Refuge with Allah from the Accursed Shaitan</title>
 </head>
 <body style="text-align: center;">
-	<img src="{{URL::asset('/image/bismillah.jpg')}}" alt="Bismillah" height="100%" width="100%">
+	<img src="{{URL::asset('/image/bismillah.jpg')}}" alt="Bismillah" height="27%" width="27%">
 	<h1>I Seek Refuge with Allah from the Accursed Shaitan</h1>
 	<h1>In the name of Allah, The Most Gracious and The Most Merciful </h1>
 	<h2>786</h2>
